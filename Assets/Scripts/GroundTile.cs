4e9afdf28dbd1a0a4f54e9a7a04a6adef4d940ab
vense <vense.lrm@gmail.com>
2021-02-15 19:22:59 +0100

    Jeu Février Complet

---
+++ Assets/Scripts/GroundTile.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundTile : MonoBehaviour
+{
+    [SerializeField] GameObject obstaclePrefab;
+    [SerializeField] GameObject coinPrefab;
+    [SerializeField] GameObject tallObstaclePrefab;
+    [SerializeField] float tallObstacleChance = 0.2f;
+
+    GroundSpawner groundSpawner;
+    
+    private void Start()
+    {
+        groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+    }
+
+    private void OnTriggerExit (Collider other)
+    {
+        groundSpawner.SpawnTile(true);
+        Destroy(gameObject, 2);
+    }
+
+
+
+    public void SpawnObstacle ()
+    {
+        GameObject obstacleToSpawn = obstaclePrefab;
+        float random = Random.Range(0f, 1f);
+        if (random < tallObstacleChance)
+        {
+            obstacleToSpawn = tallObstaclePrefab;
+        }
+
+        int obstacleSpawnIndex = Random.Range(2, 5);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+
+        Instantiate(obstacleToSpawn, spawnPoint.position, Quaternion.identity, transform);
+    }
+
+
+    public void SpawnCoins ()
+    {
+        int coinsToSpawn = 10;
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            GameObject temp = Instantiate(coinPrefab, transform);
+            temp.transform.position = GetRandomPointInCollider(GetComponent<Collider>());
+        }
+    }
+    
+    Vector3 GetRandomPointInCollider (Collider collider)
+    {
+        Vector3 point = new Vector3(
+            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+            );
+
+        if (point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPointInCollider(collider);
+        }
+
+        point.y = 1;
+        return point;
+    }
+
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit ();
+    }
+
+}
